243d35c170f9b1c808a9b1a49748ebb3f33f5526
ahizqil <ahmad.hizqil@gmail.com>
2013-12-26 12:56:40 +0700

    before realtion count

--- application/views/subproject/edit.php
+++ application/views/subproject/edit.php
@@ -2,10 +2,11 @@
   $pekerjaan_row = $this->get_data->get_pekerjaan_row();
   $count_sub = 0;
   for ($pekerjaan_id=2; $pekerjaan_id <= $pekerjaan_row+1; $pekerjaan_id++) { 
-    $subpekerjaan_row = $this->get_data->get_project_subpekerjaan_row("pekerjaan_id", $pekerjaan_id, $id_project);
-      for ($subpekerjaan_id=1; $subpekerjaan_id <= $subpekerjaan_row ; $subpekerjaan_id++) { 
+    $subpekerjaan_row = $this->get_data->get_project_subpekerjaan_row("pekerjaan_id", $pekerjaan_id, $id_project);?>
+
+      <?php for ($subpekerjaan_id=1; $subpekerjaan_id <= $subpekerjaan_row ; $subpekerjaan_id++) { 
         $count_sub+=1;
-        ?>
+      ?>
         <script>
         function sumup<?=$count_sub;?>() {
           document.form.jumlah_<?=$pekerjaan_id;?>_<?=$subpekerjaan_id;?>.value = (document.form.harga_satuan<?=$count_sub;?>.value -0) * (document.form.volume<?=$count_sub;?>.value -0);
@@ -19,10 +20,20 @@
           document.form.sum_work<?=$pekerjaan_id;?>.value = data<?=$pekerjaan_id;?>.reduce(function(pv, cv){return parseInt(pv)+parseInt(cv);});
         }
         </script>
+
       <?php }
   }
-
 ?>
+<script>
+  function call() {
+    totali = [];
+      <?php for ($i=2; $i <= $pekerjaan_row+1 ; $i++) { ?>
+        totali[<?=$i;?>] = document.form.sum_work<?=$i;?>.value
+      <?php } ?>
+      document.form.total.value = totali.reduce(function(pv, cv){return parseInt(pv)+parseInt(cv);});
+  }
+</script>
+
 <h3>Daftar Subpekerjaan</h3>
 <?=form_open('subproject/rab/'.$id_project,'name="form"');?>
 <?php 
@@ -63,12 +74,12 @@
       <td></td>
       <td></td>
       <td></td>
-      <td>Subotal</td>
+      <td>Subtotal</td>
       
-      <td><input id="sum_work<?=$pekerjaan_ids;?>" type="text" value="" class="input-small" onclick="count_sub<?=$pekerjaan_ids;?>()" ></input></td>
+      <td><input id="sum_work<?=$pekerjaan_ids;?>" type="text" value="0" class="input-small" onclick="count_sub<?=$pekerjaan_ids;?>()" ></input></td>
     </tr>
 </table>
 <?php } ?>
-<h2>Total semua pembelanjaan adalah : Rp xxx.xxx,xx </h2>
+<h2>Total semua pembelanjaan adalah : Rp <input id="total" type="text" name="total" onclick="call()" value=""></input></h2>
 <button type="submit" class="btn btn-primary">Submit</button>
 <?php form_close(); ?>
