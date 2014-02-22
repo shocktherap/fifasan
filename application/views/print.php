364b41147ecd2dcb4fa3b67f46044fd0d9a74421
shocktherap <moccaify@gmail.com>
2014-02-23 00:35:31 +0700

    database

---
+++ application/views/print.php
@@ -0,0 +1,25 @@
+<html>
+  <head>
+    <title>Print PDF Output</title>
+  </head>
+  <body>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <td>No</td>
+            <td>Jenis Pengeluaran</td>
+            <td>Nominal</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td></td>
+            <td>Sesuatu pengeluaran </td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </body>
+</html>
